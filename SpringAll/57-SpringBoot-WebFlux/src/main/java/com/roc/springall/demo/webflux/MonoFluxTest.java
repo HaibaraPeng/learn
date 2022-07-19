af4c95cdb70e2a5031aac5fbced5e517c4493aed
dongp <dongp@laschain.com>
2022-07-19 18:25:42 +0800

    springall springboot 57

---
+++ SpringAll/57-SpringBoot-WebFlux/src/main/java/com/roc/springall/demo/webflux/MonoFluxTest.java
@@ -0,0 +1,53 @@
+package com.roc.springall.demo.webflux;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description MonoFluxTest
+ * @Author dongp
+ * @Date 2022/7/19 0019 17:53
+ */
+public class MonoFluxTest {
+    public static void main(String[] args) {
+        Subscriber<Integer> subscriber = new Subscriber<Integer>() {
+            private Subscription subscription;
+
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                this.subscription = subscription;
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("接收到数据：" + integer);
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+                this.subscription.cancel();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("处理完了!");
+            }
+        };
+
+        String[] strs = {"1", "2", "3"};
+        Flux.fromArray(strs).map(Integer::parseInt).subscribe(subscriber);
+        Mono.fromSupplier(() -> 1).map(s -> s + 1).subscribe(subscriber);
+    }
+}
