70365a15ff6802178e84db5072347ad842e214f6
dongp <dongp@laschain.com>
2022-07-19 14:10:35 +0800

    springall springboot config

---
+++ SpringAll/02-SpringBoot-Config/src/main/java/com/roc/springall/demo/Demo2Application.java
@@ -0,0 +1,16 @@
+package com.roc.springall.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description Demo2Application
+ * @Author dongp
+ * @Date 2022/7/19 0019 13:46
+ */
+@SpringBootApplication
+public class Demo2Application {
+    public static void main(String[] args) {
+        new SpringApplication(Demo2Application.class).run(args);
+    }
+}
