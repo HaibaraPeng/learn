ef917af65cf3ee4ccedf656385e46a1d2aeaea9d
dongp <dongp@laschain.com>
2022-07-21 14:29:49 +0800

    springall springboot 35

--- SpringAll/35-SpringBoot-Security-Authentication/src/main/java/com/roc/springall/demo/config/BrowserSecurityConfig.java
+++ SpringAll/35-SpringBoot-Security-Authentication/src/main/java/com/roc/springall/demo/config/BrowserSecurityConfig.java
@@ -1,8 +1,14 @@
 package com.roc.springall.demo.config;
 
+import com.roc.springall.demo.handler.MyAuthenticationFailureHandler;
+import com.roc.springall.demo.handler.MyAuthenticationSucessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @Description BrowserSecurityConfig
@@ -11,12 +17,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyAuthenticationSucessHandler authenticationSucessHandler;
+
+    @Autowired
+    private MyAuthenticationFailureHandler authenticationFailureHandler;
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
+        http.formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/login")
+                .successHandler(authenticationSucessHandler)
+                .failureHandler(authenticationFailureHandler)
                 .and()
                 .authorizeRequests()
+                .antMatchers("/authentication/require", "/login.html").permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and().csrf().disable();
     }
 }
