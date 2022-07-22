d8f321f76b9d2a9c5fe8754f5c275a836b37382b
dongp <dongp@laschain.com>
2022-07-22 16:35:20 +0800

    springall springboot 66

---
+++ SpringAll/66-SpringBoot-OAuth2-SSO/sso-server/src/main/java/com/roc/springall/demo/SsoServerApplication.java
@@ -0,0 +1,15 @@
+package com.roc.springall.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * @author MrBird
+ */
+@SpringBootApplication
+public class SsoServerApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SsoServerApplication.class).run(args);
+    }
+}
