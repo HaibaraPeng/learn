e3104af4b149e0b35cef0454d22944f1321ef81c
dongp <dongp@laschain.com>
2022-07-22 15:06:15 +0800

    springall springboot 65

---
+++ SpringAll/65-SpringBoot-OAuth2-Config/src/main/java/com/roc/springall/demo/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.roc.springall.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Description SecurityConfig
+ * @Author dongp
+ * @Date 2022/7/22 0022 14:21
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
