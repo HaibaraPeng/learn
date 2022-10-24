146aaaeed22a1d026beed83e636e8666549952a4
dongp <dongp@laschain.com>
2022-10-24 14:12:48 +0800

    feat:blog-初始化项目

---
+++ blog-springboot/src/main/java/org/example/blog/BlogApplication.java
@@ -0,0 +1,17 @@
+package org.example.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description BlogApplication
+ * @Author dongp
+ * @Date 2022/10/24 0024 14:06
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
