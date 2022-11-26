d0a6d5c997e5b891bbee637651a43565be8c8af4
roc <514178739@qq.com>
2022-11-26 15:43:59 +0800

    feat:mall4j-获取产品所有分类

---
+++ mall4j/yami-shop-service/src/main/java/com/roc/shop/service/dao/CategoryMapper.java
@@ -0,0 +1,12 @@
+package com.roc.shop.service.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.roc.shop.bean.model.Category;
+
+/**
+ * @Description CategoryMapper
+ * @Author roc
+ * @Date 2022/11/26 下午3:40
+ */
+public interface CategoryMapper extends BaseMapper<Category> {
+}
---
+++ mall4j/yami-shop-service/src/main/java/com/roc/shop/service/service/CategoryService.java
@@ -0,0 +1,12 @@
+package com.roc.shop.service.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.roc.shop.bean.model.Category;
+
+/**
+ * @Description CategoryService
+ * @Author roc
+ * @Date 2022/11/26 下午3:35
+ */
+public interface CategoryService extends IService<Category> {
+}
