369c681f739b2c713b59dda1e469434f740a083a
roc <514178739@qq.com>
2022-11-26 16:14:13 +0800

    feat:mall4j-根据获取规格值最大的自增id

--- mall4j/yami-shop-admin/src/main/java/com/roc/shop/admin/controller/SpecController.java
+++ mall4j/yami-shop-admin/src/main/java/com/roc/shop/admin/controller/SpecController.java
@@ -3,6 +3,7 @@ package com.roc.shop.admin.controller;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.roc.shop.bean.enums.ProdPropRule;
 import com.roc.shop.bean.model.ProdProp;
+import com.roc.shop.bean.model.ProdPropValue;
 import com.roc.shop.security.admin.util.SecurityUtils;
 import com.roc.shop.service.service.ProdPropService;
 import com.roc.shop.service.service.ProdPropValueService;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @Description 规格管理
@@ -37,5 +39,15 @@ public class SpecController {
                         .eq(ProdProp::getRule, ProdPropRule.SPEC.value())
                         .eq(ProdProp::getShopId, SecurityUtils.getSysUser().getShopId()));
         return ResponseEntity.ok(list);
+    }
+
+    /**
+     * 根据获取规格值最大的自增id
+     */
+    @GetMapping("/listSpecMaxValueId")
+    public ResponseEntity<Long> listSpecMaxValueId() {
+        ProdPropValue propValue = prodPropValueService.getOne(new LambdaQueryWrapper<ProdPropValue>()
+                .orderByDesc(ProdPropValue::getValueId).last("limit 1"));
+        return ResponseEntity.ok(Objects.isNull(propValue) ? 0L : propValue.getValueId());
     }
 }
