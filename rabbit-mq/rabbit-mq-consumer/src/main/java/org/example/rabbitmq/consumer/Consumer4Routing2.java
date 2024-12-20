82b5f452975204f858f8066c80ccffd69177842e
Roc <514178739@qq.com>
2024-12-20 21:53:34 +0800

    feat:新增DIRECT模式消费者

---
+++ rabbit-mq/rabbit-mq-consumer/src/main/java/org/example/rabbitmq/consumer/Consumer4Routing2.java
@@ -0,0 +1,33 @@
+package org.example.rabbitmq.consumer;
+
+import com.rabbitmq.client.*;
+import org.example.rabbitmq.common.ConnectionUtil;
+
+import java.io.IOException;
+
+public class Consumer4Routing2 {
+
+    private static final String QUEUE_NAME = "test_direct_queue2";
+
+    public static void main(String[] args) throws Exception {
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+
+        DefaultConsumer consumer = new DefaultConsumer(channel) {
+
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("consumerTag：" + consumerTag);
+                System.out.println("Exchange：" + envelope.getExchange());
+                System.out.println("RoutingKey：" + envelope.getRoutingKey());
+                System.out.println("properties：" + properties);
+                System.out.println("body：" + new String(body));
+                System.out.println("Consumer3PubSub1--------------------------------");
+            }
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+}
