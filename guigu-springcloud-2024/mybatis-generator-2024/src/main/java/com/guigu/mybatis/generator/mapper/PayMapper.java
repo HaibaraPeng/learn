fcc94692bf9c4efffec863c23b9ccda3e469e6cf
Roc <514178739@qq.com>
2024-12-24 22:09:04 +0800

    feat:guigu-springcloud增加mybatis-generator模块

---
+++ guigu-springcloud-2024/mybatis-generator-2024/src/main/java/com/guigu/mybatis/generator/mapper/PayMapper.java
@@ -0,0 +1,7 @@
+package com.guigu.mybatis.generator.mapper;
+
+import com.guigu.mybatis.generator.entities.Pay;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface PayMapper extends Mapper<Pay> {
+}
