36e12280aa9b1e6891849cf9e1e0cc8a1b72a6b2
Roc <514178739@qq.com>
2024-12-25 22:16:10 +0800

    feat:guigu-springcloud Payment8001模块增加consul

--- guigu-springcloud-2024/cloud-provider-payment-8001/src/main/java/com/guigu/cloud/payment/Payment8001Application.java
+++ guigu-springcloud-2024/cloud-provider-payment-8001/src/main/java/com/guigu/cloud/payment/Payment8001Application.java
@@ -2,12 +2,14 @@ package com.guigu.cloud.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.guigu.cloud.payment.mapper") //import tk.mybatis.spring.annotation.MapperScan;
-//@EnableDiscoveryClient
-//@RefreshScope // 动态刷新
+@EnableDiscoveryClient
+@RefreshScope // 动态刷新
 public class Payment8001Application {
     public static void main(String[] args) {
         SpringApplication.run(Payment8001Application.class, args);
