391e2c2098499dd15f8c380bb799f5152b42ef92
Roc <dongpeng@aeke.com>
2024-12-25 11:40:02 +0800

    feat:增加尚硅谷-ssyx项目acl模块 PermissionController

---
+++ guigu-ssyx/guigu-ssyx-parent/guigu-ssyx-service/guigu-ssyx-service-acl/src/main/java/com/guigu/ssyx/service/acl/mapper/PermissionMapper.java
@@ -0,0 +1,11 @@
+package com.guigu.ssyx.service.acl.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.guigu.ssyx.model.entity.acl.Permission;
+
+/**
+ * @Author Roc
+ * @Date 2024/12/25 11:36
+ */
+public interface PermissionMapper extends BaseMapper<Permission> {
+}
