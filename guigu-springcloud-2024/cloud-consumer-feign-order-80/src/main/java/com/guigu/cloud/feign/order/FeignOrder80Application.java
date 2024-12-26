643a5fd0c018a0d39ea310230a3e2fe7440f34c7
Roc <514178739@qq.com>
2024-12-26 22:17:45 +0800

    feat:cloud-consumer-openfeign-order OrderController

--- guigu-springcloud-2024/cloud-consumer-feign-order-80/src/main/java/com/guigu/cloud/feign/order/FeignOrder80Application.java
+++ guigu-springcloud-2024/cloud-consumer-feign-order-80/src/main/java/com/guigu/cloud/feign/order/FeignOrder80Application.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RefreshScope // 动态刷新
+@EnableFeignClients(basePackages = {"com.guigu.cloud.common.apis"})
 public class FeignOrder80Application {
     public static void main(String[] args) {
         SpringApplication.run(FeignOrder80Application.class, args);
