3be12cef3251b8a13a9804a32723578f11ddfca1
Roc <514178739@qq.com>
2025-01-01 18:03:15 +0800

    feat:cloud-alibaba-config-nacos-client

---
+++ guigu-springcloud-2024/cloud-alibaba-config-nacos-client-3377/src/main/java/com/guigu/cloud/alibaba/config/client/ConfigClient3377Application.java
@@ -0,0 +1,11 @@
+package com.guigu.cloud.alibaba.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient3377Application {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3377Application.class, args);
+    }
+}
