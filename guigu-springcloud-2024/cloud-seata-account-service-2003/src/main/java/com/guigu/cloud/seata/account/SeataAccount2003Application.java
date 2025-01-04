92ea6385e02bce529c316edf2e1a1b8cb69d0135
Roc <514178739@qq.com>
2025-01-04 22:23:53 +0800

    feat:初始化seata-account模块

---
+++ guigu-springcloud-2024/cloud-seata-account-service-2003/src/main/java/com/guigu/cloud/seata/account/SeataAccount2003Application.java
@@ -0,0 +1,17 @@
+package com.guigu.cloud.seata.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("com.atguigu.cloud.seata.account.mapper") //import tk.mybatis.spring.annotation.MapperScan;
+@EnableDiscoveryClient //服务注册和发现
+@EnableFeignClients(basePackages = {"com.guigu.cloud.common.apis"})
+public class SeataAccount2003Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SeataAccount2003Application.class, args);
+    }
+}
