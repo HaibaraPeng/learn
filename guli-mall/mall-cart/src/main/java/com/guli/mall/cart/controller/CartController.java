db7b24b881d7135331685d78799c03c7b951a34b
Roc <514178739@qq.com>
2025-01-05 14:51:42 +0800

    feat:外部引入Swagger2Config

--- guli-mall/mall-cart/src/main/java/com/guli/mall/cart/controller/CartController.java
+++ guli-mall/mall-cart/src/main/java/com/guli/mall/cart/controller/CartController.java
@@ -6,6 +6,7 @@ import com.guli.mall.cart.vo.CartVo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -19,6 +20,7 @@ import java.util.concurrent.ExecutionException;
  * @Date 2025/01/05 13:47
  */
 @Controller
+@RequestMapping("/cart")
 public class CartController {
 
     @Resource
