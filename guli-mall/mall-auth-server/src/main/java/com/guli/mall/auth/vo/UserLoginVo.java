ef8c670ce43378defd99821b26090fc1ff8b9be8
Roc <514178739@qq.com>
2025-01-05 14:55:40 +0800

    feat:auth增加LoginController

---
+++ guli-mall/mall-auth-server/src/main/java/com/guli/mall/auth/vo/UserLoginVo.java
@@ -0,0 +1,15 @@
+package com.guli.mall.auth.vo;
+
+import lombok.Data;
+
+/**
+ * @Author Roc
+ * @Date 2025/01/05 14:53
+ */
+@Data
+public class UserLoginVo {
+
+    private String loginacct;
+
+    private String password;
+}
