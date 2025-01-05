6ff977ed1eda986899b4071d9020733c86264422
Roc <514178739@qq.com>
2025-01-05 14:57:00 +0800

    feat:auth增加swagger

--- guli-mall/mall-common/src/main/java/com/guli/mall/common/config/Swagger2Config.java
+++ guli-mall/mall-common/src/main/java/com/guli/mall/common/config/Swagger2Config.java
@@ -26,6 +26,35 @@ import java.util.List;
 public class Swagger2Config {
 
     @Bean
+    public Docket authApiConfig() {
+        List<Parameter> pars = new ArrayList<>();
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        tokenPar.name("userId")
+                .description("token")
+                //.defaultValue(JwtHelper.createToken(1L, "admin"))
+                .defaultValue("1")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+        pars.add(tokenPar.build());
+
+        Docket cartApi = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("authApi")
+                .apiInfo(apiInfo())
+                .select()
+                //只显示api路径下的页面
+                // /api/user/login
+                // /admin/order/findAll
+                // /add/PERSON/all
+                .apis(RequestHandlerSelectors.basePackage("com.guli.mall.auth"))
+//                .paths(PathSelectors.regex("/cart/.*"))
+                .build()
+                .globalOperationParameters(pars);
+        return cartApi;
+    }
+
+    @Bean
     public Docket cartApiConfig() {
         List<Parameter> pars = new ArrayList<>();
         ParameterBuilder tokenPar = new ParameterBuilder();
@@ -41,7 +70,7 @@ public class Swagger2Config {
 
         Docket cartApi = new Docket(DocumentationType.SWAGGER_2)
                 .groupName("cartApi")
-                .apiInfo(cartApiInfo())
+                .apiInfo(apiInfo())
                 .select()
                 //只显示api路径下的页面
                 // /api/user/login
@@ -54,7 +83,7 @@ public class Swagger2Config {
         return cartApi;
     }
 
-    private ApiInfo cartApiInfo() {
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("网站-API文档")
                 .description("本文档描述了谷粒商城微服务接口定义")
