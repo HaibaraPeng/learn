5c129cf58d51b4da7fbaf59f7cf1ad92a2be8583
Roc <dongpeng@aeke.com>
2025-01-06 11:28:26 +0800

    feat:Gateway项目增加CorsConfig

---
+++ guigu-ssyx/guigu-ssyx-parent/guigu-ssyx-gateway/src/main/java/com/guigu/ssyx/gateway/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.guigu.ssyx.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @Author Roc
+ * @Date 2025/1/6 11:27
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
