2b6bcbdb5abedbc83a29c302542e0fcad1e1cac0
Roc <514178739@qq.com>
2025-01-12 22:49:08 +0800

    feat:增加Sentinel限流

---
+++ ruoyi-cloud-3.6.5/ruoyi-gateway/src/main/java/com/ruoyi/cloud/gateway/config/GatewayConfig.java
@@ -0,0 +1,45 @@
+package com.ruoyi.cloud.gateway.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.ruoyi.cloud.gateway.handler.SentinelFallbackHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Author Roc
+ * @Date 2025/01/12 22:24
+ */
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelFallbackHandler sentinelGatewayExceptionHandler() {
+        return new SentinelFallbackHandler();
+    }
+
+    @PostConstruct
+    public void doInit() {
+        // 加载网关限流规则
+        initGatewayRules();
+    }
+
+    /**
+     * 网关限流规则
+     */
+    private void initGatewayRules() {
+        Set<GatewayFlowRule> rules = new HashSet<>();
+        rules.add(new GatewayFlowRule("ruoyi-auth")
+                .setCount(3) // 限流阈值
+                .setIntervalSec(60)); // 统计时间窗口，单位是秒，默认是 1 秒
+        // 加载网关限流规则
+        GatewayRuleManager.loadRules(rules);
+    }
+}
