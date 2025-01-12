66584846e474708b10ecfeea2e47da52dbc3895a
Roc <514178739@qq.com>
2025-01-12 16:37:49 +0800

    feat:auth增加sentinel实现降级

--- ruoyi-cloud-3.6.5/ruoyi-auth/src/main/java/com/ruoyi/cloud/auth/AuthApplication.java
+++ ruoyi-cloud-3.6.5/ruoyi-auth/src/main/java/com/ruoyi/cloud/auth/AuthApplication.java
@@ -4,10 +4,8 @@ import com.ruoyi.cloud.common.security.annotation.EnableRyFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableRyFeignClients
-@EnableFeignClients(basePackages = {"com.ruoyi.cloud.api.system"})
+@EnableRyFeignClients
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AuthApplication {
     public static void main(String[] args) {
