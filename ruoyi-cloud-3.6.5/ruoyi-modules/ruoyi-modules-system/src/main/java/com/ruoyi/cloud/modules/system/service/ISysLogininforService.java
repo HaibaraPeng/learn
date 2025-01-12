6a090f6894a94b8822b5654334c1243c0dd693c0
Roc <514178739@qq.com>
2025-01-12 17:36:55 +0800

    feat:system新增SysLogininforController

---
+++ ruoyi-cloud-3.6.5/ruoyi-modules/ruoyi-modules-system/src/main/java/com/ruoyi/cloud/modules/system/service/ISysLogininforService.java
@@ -0,0 +1,39 @@
+package com.ruoyi.cloud.modules.system.service;
+
+import com.ruoyi.cloud.api.system.domain.SysLogininfor;
+
+import java.util.List;
+
+/**
+ * @Author Roc
+ * @Date 2025/01/12 17:30
+ */
+public interface ISysLogininforService {
+    /**
+     * 新增系统登录日志
+     *
+     * @param logininfor 访问日志对象
+     */
+    public int insertLogininfor(SysLogininfor logininfor);
+
+//    /**
+//     * 查询系统登录日志集合
+//     *
+//     * @param logininfor 访问日志对象
+//     * @return 登录记录集合
+//     */
+//    public List<SysLogininfor> selectLogininforList(SysLogininfor logininfor);
+//
+//    /**
+//     * 批量删除系统登录日志
+//     *
+//     * @param infoIds 需要删除的登录日志ID
+//     * @return 结果
+//     */
+//    public int deleteLogininforByIds(Long[] infoIds);
+//
+//    /**
+//     * 清空系统登录日志
+//     */
+//    public void cleanLogininfor();
+}
