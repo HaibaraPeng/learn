09e2f59a6a2226c5d2af4551bc870bb2252a5c87
Roc <514178739@qq.com>
2025-01-12 22:23:23 +0800

    feat:增加XssFilter

---
+++ ruoyi-cloud-3.6.5/ruoyi-gateway/src/main/java/com/ruoyi/cloud/gateway/config/properties/XssProperties.java
@@ -0,0 +1,43 @@
+package com.ruoyi.cloud.gateway.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author Roc
+ * @Date 2025/01/12 22:15
+ */
+@Configuration
+@RefreshScope
+@ConfigurationProperties(prefix = "security.xss")
+public class XssProperties {
+    /**
+     * Xss开关
+     */
+    private Boolean enabled;
+
+    /**
+     * 排除路径
+     */
+    private List<String> excludeUrls = new ArrayList<>();
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<String> getExcludeUrls() {
+        return excludeUrls;
+    }
+
+    public void setExcludeUrls(List<String> excludeUrls) {
+        this.excludeUrls = excludeUrls;
+    }
+}
