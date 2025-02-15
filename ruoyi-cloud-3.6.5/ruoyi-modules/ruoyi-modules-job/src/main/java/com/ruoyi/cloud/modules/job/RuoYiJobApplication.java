5ce239f68c0ff867223d727d44b1d023f5958192
Roc <514178739@qq.com>
2025-02-15 22:39:45 +0800

    feat:初始job模块

---
+++ ruoyi-cloud-3.6.5/ruoyi-modules/ruoyi-modules-job/src/main/java/com/ruoyi/cloud/modules/job/RuoYiJobApplication.java
@@ -0,0 +1,19 @@
+package com.ruoyi.cloud.modules.job;
+
+import com.ruoyi.cloud.common.security.annotation.EnableCustomConfig;
+import com.ruoyi.cloud.common.security.annotation.EnableRyFeignClients;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Roc
+ * @Date 2025/02/15 22:34
+ */
+@EnableCustomConfig
+@EnableRyFeignClients
+@SpringBootApplication
+public class RuoYiJobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RuoYiJobApplication.class, args);
+    }
+}
