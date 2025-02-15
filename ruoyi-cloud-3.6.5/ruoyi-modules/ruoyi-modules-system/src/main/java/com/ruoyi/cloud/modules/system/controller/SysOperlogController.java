1fceceb9b7499817039ac55f2547090bca9e97d3
Roc <514178739@qq.com>
2025-02-15 22:17:52 +0800

    feat:新增 操作日志相关 接口

--- ruoyi-cloud-3.6.5/ruoyi-modules/ruoyi-modules-system/src/main/java/com/ruoyi/cloud/modules/system/controller/SysOperlogController.java
+++ ruoyi-cloud-3.6.5/ruoyi-modules/ruoyi-modules-system/src/main/java/com/ruoyi/cloud/modules/system/controller/SysOperlogController.java
@@ -1,9 +1,14 @@
 package com.ruoyi.cloud.modules.system.controller;
 
 import com.ruoyi.cloud.api.system.domain.SysOperLog;
+import com.ruoyi.cloud.common.core.utils.poi.ExcelUtil;
 import com.ruoyi.cloud.common.core.web.controller.BaseController;
 import com.ruoyi.cloud.common.core.web.domain.AjaxResult;
+import com.ruoyi.cloud.common.core.web.page.TableDataInfo;
+import com.ruoyi.cloud.common.log.annotation.Log;
+import com.ruoyi.cloud.common.log.enums.BusinessType;
 import com.ruoyi.cloud.common.security.annotation.InnerAuth;
+import com.ruoyi.cloud.common.security.annotation.RequiresPermissions;
 import com.ruoyi.cloud.modules.system.service.ISysOperLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,37 +26,37 @@ public class SysOperlogController extends BaseController {
     @Autowired
     private ISysOperLogService operLogService;
 
-//    @RequiresPermissions("system:operlog:list")
-//    @GetMapping("/list")
-//    public TableDataInfo list(SysOperLog operLog) {
-//        startPage();
-//        List<SysOperLog> list = operLogService.selectOperLogList(operLog);
-//        return getDataTable(list);
-//    }
-//
-//    @Log(title = "操作日志", businessType = BusinessType.EXPORT)
-//    @RequiresPermissions("system:operlog:export")
-//    @PostMapping("/export")
-//    public void export(HttpServletResponse response, SysOperLog operLog) {
-//        List<SysOperLog> list = operLogService.selectOperLogList(operLog);
-//        ExcelUtil<SysOperLog> util = new ExcelUtil<SysOperLog>(SysOperLog.class);
-//        util.exportExcel(response, list, "操作日志");
-//    }
-//
-//    @Log(title = "操作日志", businessType = BusinessType.DELETE)
-//    @RequiresPermissions("system:operlog:remove")
-//    @DeleteMapping("/{operIds}")
-//    public AjaxResult remove(@PathVariable Long[] operIds) {
-//        return toAjax(operLogService.deleteOperLogByIds(operIds));
-//    }
-//
-//    @RequiresPermissions("system:operlog:remove")
-//    @Log(title = "操作日志", businessType = BusinessType.CLEAN)
-//    @DeleteMapping("/clean")
-//    public AjaxResult clean() {
-//        operLogService.cleanOperLog();
-//        return success();
-//    }
+    @RequiresPermissions("system:operlog:list")
+    @GetMapping("/list")
+    public TableDataInfo list(SysOperLog operLog) {
+        startPage();
+        List<SysOperLog> list = operLogService.selectOperLogList(operLog);
+        return getDataTable(list);
+    }
+
+    @Log(title = "操作日志", businessType = BusinessType.EXPORT)
+    @RequiresPermissions("system:operlog:export")
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, SysOperLog operLog) {
+        List<SysOperLog> list = operLogService.selectOperLogList(operLog);
+        ExcelUtil<SysOperLog> util = new ExcelUtil<SysOperLog>(SysOperLog.class);
+        util.exportExcel(response, list, "操作日志");
+    }
+
+    @Log(title = "操作日志", businessType = BusinessType.DELETE)
+    @RequiresPermissions("system:operlog:remove")
+    @DeleteMapping("/{operIds}")
+    public AjaxResult remove(@PathVariable Long[] operIds) {
+        return toAjax(operLogService.deleteOperLogByIds(operIds));
+    }
+
+    @RequiresPermissions("system:operlog:remove")
+    @Log(title = "操作日志", businessType = BusinessType.CLEAN)
+    @DeleteMapping("/clean")
+    public AjaxResult clean() {
+        operLogService.cleanOperLog();
+        return success();
+    }
 
     @InnerAuth
     @PostMapping
--- ruoyi-cloud-3.6.5/ruoyi-modules/ruoyi-modules-system/src/main/java/com/ruoyi/cloud/modules/system/service/ISysOperLogService.java
+++ ruoyi-cloud-3.6.5/ruoyi-modules/ruoyi-modules-system/src/main/java/com/ruoyi/cloud/modules/system/service/ISysOperLogService.java
@@ -17,22 +17,22 @@ public interface ISysOperLogService {
      */
     public int insertOperlog(SysOperLog operLog);
 
-//    /**
-//     * 查询系统操作日志集合
-//     *
-//     * @param operLog 操作日志对象
-//     * @return 操作日志集合
-//     */
-//    public List<SysOperLog> selectOperLogList(SysOperLog operLog);
-//
-//    /**
-//     * 批量删除系统操作日志
-//     *
-//     * @param operIds 需要删除的操作日志ID
-//     * @return 结果
-//     */
-//    public int deleteOperLogByIds(Long[] operIds);
-//
+    /**
+     * 查询系统操作日志集合
+     *
+     * @param operLog 操作日志对象
+     * @return 操作日志集合
+     */
+    public List<SysOperLog> selectOperLogList(SysOperLog operLog);
+
+    /**
+     * 批量删除系统操作日志
+     *
+     * @param operIds 需要删除的操作日志ID
+     * @return 结果
+     */
+    public int deleteOperLogByIds(Long[] operIds);
+
 //    /**
 //     * 查询操作日志详细
 //     *
@@ -40,9 +40,9 @@ public interface ISysOperLogService {
 //     * @return 操作日志对象
 //     */
 //    public SysOperLog selectOperLogById(Long operId);
-//
-//    /**
-//     * 清空操作日志
-//     */
-//    public void cleanOperLog();
+
+    /**
+     * 清空操作日志
+     */
+    public void cleanOperLog();
 }
