f7f3f93f34620a3c6d8e87d35a071e6674cce3cd
Roc <514178739@qq.com>
2025-02-16 16:01:31 +0800

    feat:增加monitor模块

---
+++ ruoyi-cloud-3.6.5/ruoyi-visual/ruoyi-monitor/src/main/java/com/ruoyi/cloud/visual/monitor/MonitorApplication.java
@@ -0,0 +1,17 @@
+package com.ruoyi.cloud.visual.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Roc
+ * @Date 2025/02/16 12:14
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class MonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+}
