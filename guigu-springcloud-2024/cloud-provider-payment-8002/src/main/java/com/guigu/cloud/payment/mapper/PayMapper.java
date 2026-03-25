686f68e651d3dd4fdc611ce3fe05363ae4d4e203
Roc <rocdongai@gmail.com>
2026-03-25 19:59:58 +0800

    feat: 初始化项目，并新增opencode项目地图

--- guigu-springcloud-2024/cloud-provider-payment-8002/src/main/java/com/guigu/cloud/payment/mapper/PayMapper.java
+++
@@ -1,7 +0,0 @@
-package com.guigu.cloud.payment.mapper;
-
-import com.guigu.cloud.payment.entities.Pay;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface PayMapper extends Mapper<Pay> {
-}
--- guigu-ssyx/guigu-ssyx-parent/guigu-ssyx-common/guigu-ssyx-common-util/src/main/java/com/guigu/ssyx/common/util/DateUtil.java
+++
@@ -1,8 +0,0 @@
-package com.guigu.ssyx.common.util;
-
-/**
- * @Author Roc
- * @Date 2024/12/23 16:02
- */
-public class DateUtil {
-}
